776d160bee8cb0cc3da88d1c0c8799c8e3d9e8c9
Chris Le <lechris1@msu.edu>
2019-04-23 14:40:53 -0400

    added images for musicals and basic animations for fadein

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,12 +1,85 @@
 import React, { Component } from 'react';
 // import '../css/resume.css';
-import { resumeStrings} from '../res/strings';
+import { resumeStrings } from '../res/strings';
+
+class Musical extends Component {
+    render() {
+        let fadeLeft = this.props.fadeLeft ? 'fadeInLeft' : 'fadeInRight';
+        return (
+            <div className={"animated " + fadeLeft + " delay-1s"}>
+                <p>{this.props.musical.yearlocation}<br/>
+                {this.props.musical.title}<br/>
+                {this.props.musical.instruments}</p>
+            </div>
+        );
+    }
+}
+
+// var contentWayPoint = function() {
+//     var i = 0;
+//     $('.animate-box').waypoint( function( direction ) {
+
+//         if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+            
+//             i++;
+
+//             $(this.element).addClass('item-animate');
+//             setTimeout(function(){
+
+//                 $('body .animate-box.item-animate').each(function(k){
+//                     var el = $(this);
+//                     setTimeout( function () {
+//                         var effect = el.data('animate-effect');
+//                         if ( effect === 'fadeIn') {
+//                             el.addClass('fadeIn animated');
+//                         } else if ( effect === 'fadeInLeft') {
+//                             el.addClass('fadeInLeft animated');
+//                         } else if ( effect === 'fadeInRight') {
+//                             el.addClass('fadeInRight animated');
+//                         } else {
+//                             el.addClass('fadeInUp animated');
+//                         }
+
+//                         el.removeClass('item-animate');
+//                     },  k * 200, 'easeInOutExpo' );
+//                 });
+                
+//             }, 100);
+            
+//         }
+
+//     } , { offset: '85%' } );
+// };
 
 class Resume extends Component {
     render() {
+        const ewiSettings = resumeStrings.instruments.ewi.settings.map((setting) => {
+            return (
+                <li>{setting}</li>
+            );
+        });
+
+        let fadeLeft = true;
+        const musicals = resumeStrings.musicals.map((musical) => {
+            fadeLeft = !fadeLeft;
+            return (
+                <Musical
+                    fadeLeft={fadeLeft}
+                    musical={musical}
+                />
+            );
+        });
+
         return (
             <div>
                 <p>{resumeStrings.intro.body}</p>
+                <h2>{resumeStrings.instruments.title}</h2>
+                <ul>
+                    <li>{resumeStrings.instruments.saxes}</li>
+                    <li>{resumeStrings.instruments.ewi.title}</li>
+                    <ul>{ewiSettings}</ul>
+                </ul>
+                <div>{musicals}</div>
             </div>
         );
     }
--- src/res/strings.js
+++ src/res/strings.js
@@ -43,142 +43,171 @@ const resumeStrings = {
     instruments: {
         title: `Nadia's Instruments:`,
         saxes: `Baritone, Tenor, Alto, and Soprano Saxophones`,
-        ewi: `Akai EWI 4000s with Cantabile tablet system with:`,
-        swamflutes: `SWAM Flutes (Bass Flute, Alto Flute, Flute, Piccolo)`,
-        swamclarinets: `SWAM Clarinets (Bass Clarinet, A and Bb Clarinets)`,
-        swamdoubles: `SWAM Double Reeds (Oboe,  English Horn, Bassoon, Contrabassoon)`,
-        komplete: `Native Instruments Komplete (including Kontakt, Reaktor, Massive and Absynth)`,
-        arturia: `Arturia V-Collection including (Synclavier, Fairlight CMI, Mini+Modular Moog, 
-            Roland JP8,  Oberheim SEM+Matrix12, Yamaha DX7, Prophet 5+VS, ARP 2600)`
+        ewi: {
+            title: `Akai EWI 4000s with Cantabile tablet system with:`,
+            settings: [
+                `SWAM Flutes (Bass Flute, Alto Flute, Flute, Piccolo)`,
+                `SWAM Clarinets (Bass Clarinet, A and Bb Clarinets)`,
+                `SWAM Double Reeds (Oboe,  English Horn, Bassoon, Contrabassoon)`,
+                `Native Instruments Komplete (including Kontakt, Reaktor, Massive and Absynth)`,
+                `Arturia V-Collection including (Synclavier, Fairlight CMI, Mini+Modular Moog, 
+                    Roland JP8,  Oberheim SEM+Matrix12, Yamaha DX7, Prophet 5+VS, ARP 2600)`
+            ]
+        }
     },
-    musicals: {
-        honk2: {
+    musicals: [
+        {
             yearlocation: `August 2018 - stageGR, at Calvin College's Gezon Auditorium`,
             title: `Honk!`,
-            instruments: `Reeds 1 and 2 - Alto sax and EWI (flute, piccolo, and clarinet)`
+            instruments: `Reeds 1 and 2 - Alto sax and EWI (flute, piccolo, and clarinet)`,
+            image: `honk.jpg`
         },
-        musicman: {
+        {
             yearlocation: `July 2018 - Shadblow Theatre, at Jenison Performing Arts Center`,
             title: `The Music Man`,
-            instruments: `Reed 5 - EWI (clarinet, bassoon, and bass saxophone)`
+            instruments: `Reed 5 - EWI (clarinet, bassoon, and bass saxophone)`,
+            image: `logo.svg`
         },
-        jazzcamp: {
+        {
             yearlocation: `June 2018 - Aquinas College, Art and Music Center`,
             title: `Aquinas College Jazz Camp`,
-            instruments: `Office Assistant and Camp Counselor`
+            instruments: `Office Assistant and Camp Counselor`,
+            image: `logo.svg`
         },
-        notredame: {
+        {
             yearlocation: `April 2018 - stageGR, at Calvin College's Gezon Auditorium`,
             title: `The Hunchback of Notre Dame`,
-            instruments: `Reed 1 - Soprano sax and EWI (flute, piccolo, oboe, and clarinet)`
+            instruments: `Reed 1 - Soprano sax and EWI (flute, piccolo, oboe, and clarinet)`,
+            image: `logo.svg`
         },
-        dreamcoat2: {
+        {
             yearlocation: `November 2017  - stageGR and Cornerstone University, Matthews Performing Arts Center`,
             title: `Joseph and the Amazing Technicolor Dreamcoat`,
-            instruments: `Reeds 1 and Reeds 2 combined – Alto saxophone and EWI (flute and clarinet, oboe and cor anglais)`
+            instruments: `Reeds 1 and Reeds 2 combined – Alto saxophone and EWI (flute and clarinet, oboe and cor anglais)`,
+            image: `ejoseph.jpg`
         },
-        willywonka: {
+        {
             yearlocation: `​August 2017  - stageGR, at Calvin College's Gezon Auditorium`,
             title: `Willy Wonka and the Chocolate Factory`,
-            instruments: `Reeds- EWI (flute, oboe, clarinet, bassoon, and sound effects)`
+            instruments: `Reeds- EWI (flute, oboe, clarinet, bassoon, and sound effects)`,
+            image: `wonka.jpg`
         },
-        bringiton: {
+        {
             yearlocation: `July 2017  - stageGR, at Calvin College's Gezon Auditorium`,
             title: `"Bring It On" the Musical`,
-            instruments: `Keyboard 2`
+            instruments: `Keyboard 2`,
+            image: `bio.jpg`
         },
-        spellingbee: {
+        {
             yearlocation: `May 2017  - stageGR, at Meijer Theater in the Grand Rapids Public Museum`,
             title: `The 25th Annual Putnam County Spelling Bee`,
-            instruments: `Reeds – Alto saxophone and EWI (flute, oboe, and clarinet)​`
+            instruments: `Reeds – Alto saxophone and EWI (flute, oboe, and clarinet)​`,
+            image: `putnam.jpg`
         },
-        charliebrown: {
+        {
             yearlocation: `December 2016  - stageGR, at Meijer Theater in the Grand Rapids Public Museum`,
             title: `You’re a Good Man, Charlie Brown`,
-            instruments: `Reeds – Soprano and Alto saxophones and EWI (piccolo, flute, clarinet, and sound effects)`
+            instruments: `Reeds – Soprano and Alto saxophones and EWI (piccolo, flute, clarinet, and sound effects)`,
+            image: `charlie.jpg`
         },
-        seussical2: {
+        {
             yearlocation: `October 2016  - stageGR, at Meijer Theater in the Grand Rapids Public Museum`,
             title: `Seussical the Musical`,
-            instruments: `Reeds 1  – Soprano and Alto saxophones and EWI (piccolo, flute, and clarinet)`
+            instruments: `Reeds 1  – Soprano and Alto saxophones and EWI (piccolo, flute, and clarinet)`,
+            image: `seussical.jpg`
         },
-        thirteen: {
+        {
             yearlocation: `August 2016  - stageGR, at Calvin College's Gezon Auditorium`,
             title: `"13" the Musical`,
-            instruments: `Keyboard 2 and sound effects`
+            instruments: `Keyboard 2 and sound effects`,
+            image: `r13.jpg`
         },
-        fame: {
+        {
             yearlocation: `June 2016  - stageGR, at The Ladies Literary Club Auditorium`,
             title: `FAME - the Musical`,
-            instruments: `Reed 1 – Soprano, Alto, and Tenor saxophone and EWI (clarinet, flute, and sound effects)`
+            instruments: `Reed 1 – Soprano, Alto, and Tenor saxophone and EWI (clarinet, flute, and sound effects)`,
+            image: `fame.jpg`
         },
-        msboa: {
+        {
             yearlocation: `2016 - Grand Rapids Festival of the Arts`,
             title: `MSBOA District-10 All-Star Jazz Band`,
-            instruments: `Tenor Saxophone`
+            instruments: `Tenor Saxophone`,
+            image: `logo.svg`
         },
-        intothewoods: {
+        {
             yearlocation: `February 2016  - stageGR, at The Ladies Literary Club Auditorium`,
             title: `Into The Woods`,
             instruments: `Custom – EWI (flute, piccolo, Bb and A clarinet, bassoon, trumpet, 
-                french horns, sound effects, and The Giant's dialogue)`
+                french horns, sound effects, and The Giant's dialogue)`,
+            image: `cwoods.jpg`
         },
-        lesmis: {
+        {
             yearlocation: `November 2015 - Grand Rapids City High School`,
             title: `Les Misérables`,
-            instruments: `Reed 1 book – Alto saxophone and EWI (piccolo, flute, clarinet, and musket sound effects)`
+            instruments: `Reed 1 book – Alto saxophone and EWI (piccolo, flute, clarinet, and musket sound effects)`,
+            image: `blesmiz.jpg`
         },
-        footloose2: {
+        {
             yearlocation: `July 2015  - Grand Rapids Civic Theatre`,
             title: `Footloose - the Musical`,
-            instruments: `Reed 1 book – Tenor and Bari saxophones and EWI (flute, clarinet, and Moog bass)`
+            instruments: `Reed 1 book – Tenor and Bari saxophones and EWI (flute, clarinet, and Moog bass)`,
+            image: `afootloose.jpg`
         },
-        hsm: {
+        {
             yearlocation: `June 2015 - The Vocal Music Workshop, at City High School`,
             title: `Disney’s High School Musical`,
-            instruments: `EWI (sound effects and parts of Keyboard two), Alto saxophone "street musician" walk-on role`
+            instruments: `EWI (sound effects and parts of Keyboard two), Alto saxophone "street musician" walk-on role`,
+            image: `xhsm.jpg`
         },
-        hellodolly: {
+        {
             yearlocation: `January 2015 - The Vocal Music Workshop, at City High School`,
             title: `Hello Dolly`,
-            instruments: `Reed 1 book – Alto saxophone and EWI (clarinet, flute, piccolo, and sound effects)`
+            instruments: `Reed 1 book – Alto saxophone and EWI (clarinet, flute, piccolo, and sound effects)`,
+            image: `dolly.jpg`
         },
-        honk1: {
+        {
             yearlocation: `November 2014 - Grand Rapids City High School`,
             title: `Honk!`,
-            instruments: `Reed 1 book – Alto saxophone and EWI (clarinet, flute, piccolo, and sound effects)`
+            instruments: `Reed 1 book – Alto saxophone and EWI (clarinet, flute, piccolo, and sound effects)`,
+            image: `honk.jpg`
         },
-        footloose1: {
+        {
             yearlocation: `June 2014 - The Vocal Music Workshop, at City High School`,
             title: `Footloose - the Musical`,
-            instruments: `Tenor and Bari saxophones, and EWI (clarinet and Moog bass) and EWI flute for rehearsals`
+            instruments: `Tenor and Bari saxophones, and EWI (clarinet and Moog bass) and EWI flute for rehearsals`,
+            image: `afootloose.jpg`
         },
-        howtosucceed: {
+        {
             yearlocation: `January 2014 - The Vocal Music Workshop, at City High School`,
             title: `How to Succeed in Business Without Really Trying`,
-            instruments: `Alto and Bari saxophones`
+            instruments: `Alto and Bari saxophones`,
+            image: `lbusiness.jpg`
         },
-        thisisland: {
+        {
             yearlocation: `November 2013 - Grand Rapids City High School`,
             title: `Once On This Island`,
-            instruments: `Soprano saxophone`
+            instruments: `Soprano saxophone`,
+            image: `once.jpg`
         },
-        seussical1: {
+        {
             yearlocation: `June 2013 - The Vocal Music Workshop, at Ottawa Hills High School`,
             title: `Seussical the Musical`,
-            instruments: `Alto and Tenor saxophones`
+            instruments: `Alto and Tenor saxophones`,
+            image: `seussical.jpg`
         },
-        annie: {
+        {
             yearlocation: `March 2013 - Grand Rapids City High School`,
             title: `Annie`,
-            instruments: `Alto Saxophone`
+            instruments: `Alto Saxophone`,
+            image: `kannie.jpg`
         },
-        dreamcoat1: {
+        {
             yearlocation: `November 2012 - Grand Rapids City High School`,
             title: `Joseph and The Amazing Technicolor Dreamcoat`,
-            instruments: `Alto Saxophone`
+            instruments: `Alto Saxophone`,
+            image: `ejoseph.jpg`
         }
-    }
+    ]
 }
 
 export {
